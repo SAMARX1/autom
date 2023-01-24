c5f8fa8178c5328e34ab2287f696ad71a439c053
bu2terFLY <119470972+bu2terFLY@users.noreply.github.com>
2023-01-24 18:31:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME',"samar")
 COLLECTION_NAME = environ.get('COLLECTION_NAME','Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL','1001728293498'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL',''))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT','F9Videos_Support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
